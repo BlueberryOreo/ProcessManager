96f71095d67a0eb256f61f1560d891c091715397
BlueberryOreo <1761988645@qq.com>
2024-11-20 18:18:32 +0800

    增加行计数功能，输出处理的总行数

--- lsqueue.cpp
+++ lsqueue.cpp
@@ -14,10 +14,13 @@ int main(){
          << setw(60) << "cwd"
          << setw(60) << "script" << "\t" << "status" << endl;
     if(file.is_open()){
+        int line_num = 0;
         while(getline(file, line)){
             cout << line << endl;
+            line_num ++;
         }
         file.close();
+        cout << "Total process: " << line_num << endl;
     }else{
         cout << "Failed to open file." << endl;
     }
